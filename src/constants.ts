f32ae320c1afddcfa1e1008425e93d8593ec124d
JW <mgcmasterchief@gmail.com>
2024-04-19 12:59:31 +0300

    feat: completed end game line animations;

--- src/constants.ts
+++ src/constants.ts
@@ -44,43 +44,33 @@
 export const FIELD_SIZE = 3;
 
 /* VERTICAL CHECK */
-export const ANIMATION_TEMPLATE = [
-  [
-    [0, 0, 1],
-    [0, 0, 1],
-    [0, 0, 1],
-  ],
-];
+// export const ANIMATION_TEMPLATE = [
+//   [
+//     [0, 0, 1],
+//     [0, 0, 1],
+//     [0, 0, 1],
+//   ],
+// ];
 
 /* HORIZONTAL CHECK */
 // export const ANIMATION_TEMPLATE = [
 //   [
 //     [0, 0, 0],
-//     [1, 0, 0],
 //     [0, 0, 0],
-//   ],
-//   [
-//     [0, 0, 0],
-//     [1, 0, 1],
-//     [0, 0, 0],
-//   ],
-//   [
-//     [0, 0, 0],
-//     [1, 1, 1],
-//     [0, 0, 0],
+//     [2, 2, 2],
 //   ],
 // ];
 
 /* DIAGONAL CHECK */
-// export const ANIMATION_TEMPLATE = [
-//   [
-//     [2, 0, 0],
-//     [0, 2, 0],
-//     [0, 0, 2],
-//   ],
-//   // [
-//   //   [0, 0, 1],
-//   //   [0, 1, 0],
-//   //   [1, 0, 0],
-//   // ],
-// ];
+export const ANIMATION_TEMPLATE = [
+  // [
+  //   [2, 0, 0],
+  //   [0, 2, 0],
+  //   [0, 0, 2],
+  // ],
+  [
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+  ],
+];
